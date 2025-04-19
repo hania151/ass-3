bf6d61c289c24ca358f9fcb4b85101673c9cdc32
hania151 <haniaadeel124@gmail.com>
2025-04-19 22:23:34 +0500

    Add files via upload

---
+++ ass 3.py
@@ -0,0 +1,32 @@
+total_sum=5+7+6
+difference=4-5-6
+product=5*6*3
+average=total_sum/3
+print("sum:",total_sum)
+print("difference:",difference)
+print("product:",product)
+print("average:",average)
+length=4
+width=9
+area_rectangle=length*width
+print("area of rectangle:",area_rectangle)
+import math
+radius=4.0
+area_circle=math.pi*radius ** 2
+print("area of circle:",area_circle)
+base=4
+height=8
+area_triangle=0.5*base*height
+print("area of triangle:",area_triangle)
+side=6
+side=6
+area_square=side*side
+print("area of square:",area_square)
+a=45
+b=24
+print("before swapping:")
+print("a=",a)
+print("b=",b)
+print("after swapping:")
+print("a=",b)
+print("b=",a)
